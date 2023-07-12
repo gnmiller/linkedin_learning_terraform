952b8f0f61de11f4e538fc853422b1fce6670b31
GM <gnmiller@mtu.edu>
2023-07-12 12:42:00 -0400

    lessong 03_07 sg module

--- main.tf
+++ main.tf
@@ -14,10 +14,23 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "blog_sg" {
+  source	=	"terraform-aws-modules/security-group/aws"
+  version	=	"5.1.0"
+  name	=	"blog_sg"
+  
+  vpc_id	=	data.aws_vpc.default.id
+  ingress_rules	=	["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks	=	["0.0.0.0/0"]
+  egress_rules	=	["all-all"]
+  egress_cidr_blocks	=	["0.0.0.0/0"]
+  
+}
+
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  ami           			= data.aws_ami.app_ami.id
+  instance_type 			= var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   
   tags = {
     Name = "HelloWorld"
