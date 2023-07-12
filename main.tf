5cdda42743032e5935c00a81c373f6b5720fd115
GM <gnmiller@mtu.edu>
2023-07-12 13:04:23 -0400

    implement vpc module -- troubleshooting

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 module "blog_sg" {
   source	=	"terraform-aws-modules/security-group/aws"
   version	=	"5.1.0"
-  name	=	"blog_sg"
+  name	=	"blog"
   
   vpc_id	=	module.vpc.public_subnets[0]
   ingress_rules	=	["http-80-tcp", "https-443-tcp"]
