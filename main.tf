565757f7232d665b97c156b5ca5cb9f499fdabbf
GM <gnmiller@mtu.edu>
2023-07-12 12:24:13 -0400

    lesson 03_04

--- main.tf
+++ main.tf
@@ -17,8 +17,47 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  vpc_security_group_ids = [aws_security_group.blog.id]
+  
   tags = {
     Name = "HelloWorld"
-  }
 }
+  
+data "aws_vpc" "default" {
+	default = true
+}
+  
+resource "aws_security_group" "blog" {
+  name 	= "blog-sg"
+  description	= "Allow 80/443 IN, Allow ALL OUT"
+  
+  vpc_id 		= data.aws_vpc.default.id
+}
+  
+resource "aws_security_group_rule" "blog_http_in" {
+	type 		= "ingress"
+	from_port 		= 80
+	to_port 		= 80
+	protocol		= TCP
+	cidr_blocks 		= ["0.0.0.0/0"]
+	security_group_id		= aws_security_group.blog.id
+}
+  
+resource "aws_security_group_rule" "blog_https_in" {
+	type 		= "ingress"
+	from_port 		= 443
+	to_port 		= 443
+	protocol		= TCP
+	cidr_blocks 		= ["0.0.0.0/0"]
+	security_group_id		= aws_security_group.blog.id
+}
+  
+resource "aws_security_group_rule" "blog_all_out" {
+	type 		= "egress"
+	from_port 		= 0
+	to_port 		= 0
+	protocol		= -1
+	cidr_blocks 		= ["0.0.0.0/0"]
+	security_group_id		= aws_security_group.blog.id
+}
+  
