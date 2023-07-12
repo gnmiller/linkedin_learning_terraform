e172822ead9691cdf28e15eaa8d593d254b37083
GM <gnmiller@mtu.edu>
2023-07-12 13:28:50 -0400

    implement lb

--- main.tf
+++ main.tf
@@ -37,6 +37,46 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_sg.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
   
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
